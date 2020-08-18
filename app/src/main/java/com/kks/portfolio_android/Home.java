faf721e2fc3c247e982accdd76774c6e3522e33e
Discoonect <67423146+Discoonect@users.noreply.github.com>
2020-08-18 11:57:33 +0900

    회원가입  O 로그인    O 프래그먼트 O

--- app/src/main/java/com/kks/portfolio_android/Home.java
+++ app/src/main/java/com/kks/portfolio_android/Home.java
@@ -1,16 +1,72 @@
 package com.kks.portfolio_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.kks.portfolio_android.R;
+import com.kks.portfolio_android.fragment.Fragment_Favorite;
+import com.kks.portfolio_android.fragment.Fragment_Home;
+import com.kks.portfolio_android.fragment.Fragment_Search;
+import com.kks.portfolio_android.fragment.Fragment_Setting;
+import com.kks.portfolio_android.fragment.Fragment_Write;
 
 public class Home extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+
+    private Fragment_Home fragment_home;
+    private Fragment_Search fragment_search;
+    private Fragment_Write fragment_write;
+    private Fragment_Setting fragment_setting;
+    private Fragment_Favorite fragment_favorite;
+
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fragmentManager = getSupportFragmentManager();
+
+        fragment_home = new Fragment_Home();
+        fragment_search = new Fragment_Search();
+        fragment_write = new Fragment_Write();
+        fragment_setting = new Fragment_Setting();
+        fragment_favorite = new Fragment_Favorite();
+
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,fragment_home).commitAllowingStateLoss();
+    }
+
+    public void clickHandler(View view){
+
+        transaction = fragmentManager.beginTransaction();
+
+        switch (view.getId()){
+            case R.id.btn_fragment_home:
+                transaction.replace(R.id.frameLayout,fragment_home).commitAllowingStateLoss();
+                break;
+
+            case R.id.btn_fragment_search:
+                transaction.replace(R.id.frameLayout,fragment_search).commitAllowingStateLoss();
+                break;
+
+            case R.id.btn_fragment_write:
+                transaction.replace(R.id.frameLayout,fragment_write).commitAllowingStateLoss();
+                break;
+
+            case R.id.btn_fragment_favorite:
+                transaction.replace(R.id.frameLayout,fragment_favorite).commitAllowingStateLoss();
+                break;
+
+            case R.id.btn_fragment_setting:
+                transaction.replace(R.id.frameLayout,fragment_setting).commitAllowingStateLoss();
+                break;
+        }
+
     }
 }
